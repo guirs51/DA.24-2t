80810cb1175c3535578c1515820e3e4da16d0e55
guirs51 <143645273+guirs51@users.noreply.github.com>
2024-08-19 17:06:45 -0300

    Add files via upload

---
+++ aula6/exercico.js
@@ -0,0 +1,42 @@
+const valor1 = 3
+const valor2 = 4
+const somaDosValores = valor1 + valor2
+
+console.log( somaDosValores)
+
+const valor3 = 5 
+const valor4 = 2
+
+const operaçaoEntreValores = valor1 * valor3 - valor4
+
+console.log(operaçaoEntreValores)
+
+const segundaOperaçao = valor2 - valor3 / valor4
+
+console.log(segundaOperaçao)
+
+const valor5 = 234 
+
+const terceiraOperacao = valor5 / valor3
+
+console.log( terceiraOperacao)
+
+const restoDaOperacao = 234%5
+
+console.log(restoDaOperacao)
+
+
+
+
+
+
+const num1 = 2
+const num2 = 5 
+const condicao = num1 === num2
+console.log(condicao)// resutaldo false 
+const condicao2 = num1 !== num2
+console.log(condicao2)// resultado true
+const condicao3 = num1 > num2 
+console.log(condicao3)// resultado false
+const condicao4 = num1 < num2 
+console.log(condicao4)// resultado true
---
+++ aula6/index.js
@@ -0,0 +1,80 @@
+/*  ecemplo de soma
+const primeiroValor = 10
+const segundoValor = 20
+const resultado = primeiroValor + segundoValor + 4
+
+console.log("valor do resultado é" ,resultado)//34
+*/
+
+/*  exemplo de subtraçao 
+const primeiroValor = 10
+const segundoValor = 20
+const resultado = primeiroValor - segundoValor 
+
+console.log("valor do resultado é" ,resultado)
+*/
+
+/* exemplo de multiplicaçao
+const primeiroValor = 10
+const segundoValor = 25
+const resultado = primeiroValor * segundoValor 
+console.log("valor do resultado é" ,resultado)
+*/
+
+/*  exemplo de divisao
+const primeiroValor = 345
+const segundoValor = 10
+const resultado = primeiroValor / segundoValor + 
+
+console.log("valor do resultado é" ,resultado)
+*/
+
+/* resto da divisao
+const restoDaDivisao = 11%4
+console.log("o resto da divisao é" , restoDaDivisao)
+*/
+
+// exemplos comparadores
+/*
+// comparador igual
+
+// comparaçao de valor
+const condicao = 1 === 1
+console.log(condicao)
+// comparacao de valor e tipo igual
+const condicao2 = 1 === "1"
+console.log(condicao2)
+*/
+
+/*
+// comparador diferente
+const condicao = 1  !== "1" 
+console.log(condicao)// resultado true
+const condicao2 = 1 !== 1
+console.log(condicao2)// resultado false
+const condicao3 = 2 !== 1 
+console.log(condicao3)// resultado true
+*/
+
+// maior e maior igual
+
+/*
+const condicao = 1 > 2
+console.log(condicao)// resultado false 
+const condicao2 = 2 > 2
+console.log(condicao2)// resultado false 
+const condicao3 = 3 > 2
+console.log(condicao3)// resultado false 
+const condicao4 = 2 >= 2
+console.log(condicao4)// resultado true
+*/
+
+// menor e menor igual
+const condicao =  1 < 1
+console.log(condicao)// resultado false
+const condicao2 = 2 < 1 
+console.log(condicao2)// resultado false
+const condicao3 = 1 < 2 
+console.log(condicao3)// resultado true
+const condicao4 = 1 <= 1
+console.log(condicao4)// resultado true 
