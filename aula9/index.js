8cb93de7428426eb0bd0f35fbc54fa46f8cb902b
guirs51 <143645273+guirs51@users.noreply.github.com>
2024-09-10 17:26:51 -0300

    Add files via upload

---
+++ aula9/index.js
@@ -0,0 +1,9 @@
+let pergunta = prompt("digite sua cor favorita")
+const nome = prompt("seu nome")
+console.log(`a cor favorita do user ${nome} é ${pergunta}`)
+
+console.log(" a cor favorita do user"+ nome + "é" + pergunta)
+
+const frase = prompt("uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A" , "i"), frase.length)
