5c17ec6880aacf27c7c6f68c637e5390d710fc17
guirs51 <143645273+guirs51@users.noreply.github.com>
2024-09-23 15:56:02 -0300

    Update version2.js

--- gerenciadorDePersonagemLotR/version2.js
+++ gerenciadorDePersonagemLotR/version2.js
@@ -1,56 +1,61 @@
-const listaDePersonagens = []
+const listaDePersonagens = [];
 
-let listaDeacoes = []
+let listaDeacoes = [];
 
 const personagemNUll = {
-    nome: "", 
-    class: "",
-    nivel: "",
-    habilidadeInicial: "",
-    HP: "",
-    historicoAcoes: [{ataqueEspecial: []},{ataqueNormal: []},{ defesa: []}]
+  nome: "",
+  class: "",
+  nivel: "",
+  habilidadeInicial: "",
+  HP: "",
+  historicoAcoes: [],
+};
+function criarPersonagem(personagem) {
+  const criandoPersonagem = (personagem = {
+    ...personagemNUll,
+    nome: String(prompt("digite o nome do seu personagem")),
+    class: String(prompt("digite o nome da sua classe")),
+    nivel: Number(prompt("digite o nivel do seu personagem")),
+    habilidadeInicial: String(prompt("digite a sua habilidade inicial")),
+    HP: Number(
+      prompt("digite quanto de HP(pontos de vida) tera seu persongem")
+    ),
+  });
+  listaDePersonagens.push(criandoPersonagem);
 }
-function criarPersonagem(personagem){
-    const criandoPersonagem = personagem = {
-        ...personagemNUll,
-        nome: String(prompt("digite o nome do seu personagem")),
-        class: String(prompt("digite o nome da sua classe")),
-        nivel: Number(prompt("digite o nivel do seu personagem")),
-        habilidadeInicial: String(prompt("digite a sua habilidade inicial")),
-        HP: Number(prompt("digite quanto de HP(pontos de vida) tera seu persongem"))
-    }
-    listaDePersonagens.push(criandoPersonagem)
-}
-criarPersonagem(personagemNUll)
-console.log(listaDePersonagens)
-criarPersonagem(personagemNUll)
+criarPersonagem(personagemNUll);
+console.log(listaDePersonagens);
+criarPersonagem(personagemNUll);
 
 function ataqueNormal() {
-const ataque = "voce fez um ataque normal"
-console.log(ataque)
- listaDeacoes.push(ataque)
+  const ataque = "voce fez um ataque normal";
+  console.log(ataque);
+  listaDeacoes.push(ataque);
+  personagemNUll.historicoAcoes.push(ataque);
 }
 
 function ataqueEspecial() {
-    const ataque = "voce fez um ataque especial"
-console.log(ataque)
- listaDeacoes.push(ataque)
+  const ataque = "voce fez um ataque especial";
+  console.log(ataque);
+  listaDeacoes.push(ataque);
+  personagemNUll.historicoAcoes.push(ataque);
 }
 
 function defesa() {
-    const defesa = "voce usou uma defesa"
-console.log(defesa)
- listaDeacoes.push(defesa)
+  const defesa = "voce usou uma defesa";
+  console.log(defesa);
+  listaDeacoes.push(defesa);
 }
 
-defesa()
-ataqueEspecial()
-ataqueNormal()
-console.log(listaDeacoes)
+defesa();
+ataqueEspecial();
+ataqueNormal();
+acoes(listaDePersonagens[1]);
+console.log(listaDeacoes);
 
-function acoes() {
-    personagemNUll.historicoAcoes.push(listaDeacoes)
-    console.log(listaDePersonagens)
+function acoes(listaDePersonagens) {
+  listaDePersonagens.historicoAcoes.push(defesa());
+  listaDePersonagens.historicoAcoes.push(ataqueNormal());
+  listaDePersonagens.historicoAcoes.push(ataqueEspecial());
 }
-acoes()
 
